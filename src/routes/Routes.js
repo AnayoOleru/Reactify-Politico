e1544a571a67c71d7a20657eb6978865e833aaad
Anayo Oleru <anayooleru@Anayos-MacBook-Pro.local>
2019-04-23 14:32:52 +0100

    165527873-feature(homepage): create homepage - create components for homepage - install loaders
    [Finishes 165527873]

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import homePage from '../components/pages/home-page.jsx';
+import About from '../components/pages/sign-in.jsx';
 
 export default (
   <React.Fragment>
   <Route path="/" component={homePage} />
+  <Route path="/about" component={About} />
   </React.Fragment>
 );
