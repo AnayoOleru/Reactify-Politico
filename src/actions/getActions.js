4c1ed59d1406c0b45b88678c76831f2317ea44e6
Anayo Oleru <anayo_oleru@outlook.com>
2019-04-27 14:25:32 +0100

    165644985-(user):admin get all user - add actions - add reducers - add route - add page

--- src/actions/getActions.js
+++ src/actions/getActions.js
@@ -1,4 +1,4 @@
-import { FETCH_POSTS, NEW_POST } from './types';
+import { FETCH_POSTS, NEW_POST, FETCH_USER } from './types';
 
 // signup action
 export const SignupAction = (signupData) => dispatch =>  {
@@ -18,20 +18,21 @@ export const SignupAction = (signupData) => dispatch =>  {
       }));
     };
 
-    // signup action
-export const UsersAction = (signupData) => dispatch =>  {
-    fetch('https://trustpolitico.herokuapp.com/api/v1/auth/signup',{
+    // get all users
+export const getAllUsers = (userData) => dispatch =>  {
+    fetch('https://trustpolitico.herokuapp.com/api/v1/users',{
       headers: {
           'Accept': 'application/json, text/plain, */*',
-          'Content-type': 'application/json'
+          'Content-type': 'application/json',
+          'x-access-token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImY3Yjg2NzZlLTRmMTYtNGQxZC1iYjJkLWJkOGE3MjA1ZTcyMiIsImlzQWRtaW4iOnRydWUsInVzZXJOYW1lIjoiQW5heW8iLCJsYXN0TmFtZSI6Ik9sZXJ1IiwiaWF0IjoxNTU2MzcwODIzLCJleHAiOjE1NTY0NTcyMjN9.0ZhU_epFYgQGLrtW9aDQ2biFY298k24MeXPLeUFdFHc'
         },
-        method: 'POST',
-        body: JSON.stringify(signupData)
+        method: 'GET',
+        body: JSON.stringify(userData)
     })
     .then((response) => response.json())
-    .then(posts =>
+    .then(users =>
       dispatch({
-      type: NEW_POST,
-      payload: posts
+      type: FETCH_USER,
+      payload: users
     }));
   };
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import authReducer from './authReducer';
 import postReducer from './postReducer';
+import getReducer from './getReducer';
 
 export default combineReducers({
 auth :authReducer,
-posts: postReducer
+posts: postReducer,
+get: getReducer,
 });
