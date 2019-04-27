d91430fda643a1e2435772ecd96417b313d57a9f
Anayo Oleru <anayo_oleru@outlook.com>
2019-04-27 15:43:58 +0100

    165644995-(candidates): user get all candidates - add actions - add types - add route - update reducer

--- src/actions/types.js
+++ src/actions/types.js
@@ -3,6 +3,7 @@ export const NEW_POST = 'NEW_POST';
 export const FETCH_PARTIES = 'FETCH_PARTIES';
 export const NEW_PARTY= 'NEW_PARTY';
 export const FETCH_USER = 'FETCH_USER';
+export const FETCH_CANDIDATES = 'FETCH_CANDIDATES';
 export const NEW_OFFICE = 'NEW_OFFICE';
 export const NEW_VOTE = 'NEW_VOTE';
 export const NEW_CANDIDATE = 'NEW_CANDIDATE';
---
+++ src/components/pages/candidates-page.jsx
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import NavBar from '../reuseable component/auth-user-navbar.component.jsx';
+import Card from '../reuseable component/footer-component.jsx';
+import { connect } from 'react-redux';
+import { getAllCandidates } from '../../actions/getActions';
+
+class Candidates extends Component {
+    componentWillMount(){
+        // eslint-disable-next-line react/prop-types
+        this.props.getAllCandidates();
+    }
+  render() {
+    //   here declare how you want the posts to be structured like
+      // eslint-disable-next-line react/prop-types
+      const postItems = this.props.posts.map(post => (
+        //   for example
+        <div key={post.id}>
+        <h3>{post.title}</h3>
+        <p>{post.body}</p>
+        </div>
+        
+      ))
+    return (
+        <React.Fragment>
+            <NavBar />
+            <main>
+        <section className="section-cards">
+            <div className="text-cards">
+                <h1 className="heading-primary">
+                    Parties
+                </h1>
+{/* then render the items, for example */}
+                <div>{postItems}</div>
+
+                <div className="col-1-of-3">
+                <div className="card">
+                    <div className="card__side card__side--front">
+                        <div className="card__picture card__picture--1" id="partyImage">&nbsp;</div>
+                        <div className="card__details">
+                            <ul>
+                                <li id="partyName" />
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+            <div className="row" id="partyResult" />
+
+        </section>
+    </main>
+        </React.Fragment>
+    );
+  }
+}
+// get the new data in the state, or get data from redux
+const mapStateToProps = state => ({
+    posts: state.get.items
+});
+
+export default connect(mapStateToProps, getAllCandidates)(Candidates);
