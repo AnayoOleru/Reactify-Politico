8d835a2a6b4e553aa2a78c8e511d64e58ec5f688
Anayo Oleru <anayo_oleru@outlook.com>
2019-05-16 08:08:28 +0100

    165603341-feature(user-signup): fix user signup - fix redirection for user signup

--- src/components/pages/sign-in.jsx
+++ src/components/pages/sign-in.jsx
@@ -4,16 +4,11 @@ import { Provider } from 'react-redux';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Redirect, withRouter } from 'react-router-dom';
-
-// import { css } from '@emotion/core';
-// import { ClipLoader } from 'react-spinners';
 import { createPost } from '../../actions/postActions';
 import store from '../../store';
 import SignUpNavBar from '../reuseable component/signup-navbar-component.jsx';
 import validateB4Submission from '../../validation/validateB4Submission';
 import { validateInputs } from '../../validation/validateInputs';
-
-// import '../../styles/signup.style.css';
 
 
 class SignIn extends Component {
@@ -27,7 +22,6 @@ class SignIn extends Component {
       loading: false,
     };
     this.onChange = this.onChange.bind(this);
-    // this.onSubmit = this.onSubmit.bind(this);
   }
   componentDidMount() {
     swal({
@@ -35,10 +29,6 @@ class SignIn extends Component {
       title: 'Welcome back, please sign in',
     });
   }
-
-  // componentWillReceiveProps() {
-    
-  // }
   
   ShowSpinner = () => {
     return this.setState({ loading: true });
@@ -49,11 +39,10 @@ class SignIn extends Component {
     if (result[1]) this.setState({ [result[0]]: false });
   }
   // in here before the component loads
-  // show spinner
 
   onChange(e) {
     this.setState({[e.target.name]: e.target.value});
-  }
+  };
 
   handleSubmit = async (e) => {
     e.preventDefault();
@@ -61,11 +50,12 @@ class SignIn extends Component {
     const userInput = this.state;
     const result = validateB4Submission(userInput);
     if(!result) {
-      // eslint-disable-next-line no-console
-      console.log('Inputs must be valid before submission');
+      swal({
+        icon: 'error',
+        title: 'Inputs must be valid before submission',
+      });
     }
-    // here show the spinner(add)
-    this.ShowSpinner();
+
     // clear the state
     this.setState({
       email: '',
@@ -79,7 +69,6 @@ class SignIn extends Component {
       password: userInput.password,
       };
        
-      // console.log(data, 'data');
 
     //  call action 
      this.props.createPost(data);  
@@ -87,14 +76,16 @@ class SignIn extends Component {
 
   render() {
 
-    const {token, success} = this.props;
+    const {token, success, history } = this.props;
+
+    console.log(this.props)
     if (success) {
-      console.log('success');
       swal({
         icon: 'success',
         title: 'Signin successful',
       });
-      this.props.history.push('/parties');
+      console.log('redirect');
+      history.push('/parties');
     }
     const {
       email,
@@ -102,23 +93,6 @@ class SignIn extends Component {
       isEmailError,
       isPasswordError,
     } = this.state;
-    // setup the loader
-//     const override = css`
-//     display: block;
-//     margin: 0 auto;
-//     margin-right:10px;
-//     border-color: red;
-// `;
-
-// const spinner = (<span className='sweet-loading'>
-// <ClipLoader
-//   css={override}
-//   sizeUnit={'px'}
-//   size={10}
-//   color={'white'}
-//   loading={loading}
-// />
-// </span>);
 
     return (
       <Provider store={store}>
@@ -150,13 +124,13 @@ class SignIn extends Component {
 }
 
 const mapStateToProps = ({posts}) =>({
-  success: posts.token,
+  success: posts.success,
   token: posts.token
 });
 
 SignIn.propTypes = {
   createPost:  PropTypes.func.isRequired,
-  success:  PropTypes.bool.isRequired
+  success:  PropTypes.bool
 } 
 
 export default connect(mapStateToProps,  { createPost  })(withRouter( SignIn));
