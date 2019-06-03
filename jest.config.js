82dc119f037661e9915e0b235c6cc806c5f6cc8a
Anayo Oleru <anayo_oleru@outlook.com>
2019-06-03 20:40:02 +0100

    increase test coverage

--- jest.config.js
+++ jest.config.js
@@ -18,7 +18,7 @@ module.exports = {
     coverageDirectory: 'coverage',
   
     // An array of file extensions your modules use
-    moduleFileExtensions: ['js', 'json', 'jsx'],
+    moduleFileExtensions: ['js', 'jsx', 'json'],
   
     // The paths to modules that run some code to configure or set up the testing environment before each test
     setupFiles: ['<rootDir>/src/tests/setup.jsx'],
